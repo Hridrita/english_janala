d1ea24a01363b1a5532c3fbde3d1e2ab2fe91cb2
WT_FALL-25-26- <moonsaha721@gmail.com>
2026-03-03 18:54:51 +0600

    fix some bugs

--- script/index.js
+++ script/index.js
@@ -18,6 +18,17 @@ const displayLevelWord = (words) =>{
    const wordContainer = document.getElementById("word-container");
    wordContainer.innerHTML = "";
 
+   if(words.length == 0){
+   wordContainer.innerHTML = `
+   <div class=" text-center col-span-full rounded-xl py-10 space-y-6">
+   <img class="mx-auto" src="./assets/alert-error.png"/>
+    <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+    <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+  </div>
+   `;
+    return;
+   }
+
 
    // {
 // "id": 4,
@@ -28,15 +39,15 @@ const displayLevelWord = (words) =>{
 // },
 
    words.forEach(word =>{
-    console.log(word);
+    //console.log(word);
 
     const card = document.createElement("div");
     card.innerHTML = `
       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <h2 class="font-bold text-2xl">${word.word ?  word.word : "শব্দ পাওয়া যায় নি"}</h2>
     <p class="font-semibold">Meaning/pronounciation</p>
 
-    <div class="font-semibold text-2xl font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+    <div class="font-semibold text-2xl font-bangla">"${word.meaning? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation?  word.pronunciation : "pronunciation পাওয়া যায় নি" }"</div>
     <div class="flex justify-between items-center">
       <button class="btn bg-[#1a91ff1A] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
       <button class="btn bg-[#1a91ff1A] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
